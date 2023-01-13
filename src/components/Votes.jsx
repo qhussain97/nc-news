b15b5c2d84b1bb8dcb1aa8af9cea1ff4a7e80e46
Qasim Hussain <qasim.hussain1@kpmg.co.uk>
2023-01-13 14:51:13 +0000

    Votes

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -4,35 +4,24 @@ import { patchVotesByArtcleId } from "../utils/api"
 const Votes = ({ votes, article_id }) => {
     const [votesChange, setVotesChange] = useState(0);
 
-    function incVote() {
-        setVotesChange((currVotesChange) => currVotesChange + 1);
-        patchVotesByArtcleId(article_id, 1)
-            .catch(() => {
-                setVotesChange((currVotesChange) => 
-                    currVotesChange - votesChange)
-                    alert("VOTE NOT SUBMITTED")
-                
-            })
-    }
 
-    function decVote() {
-        setVotesChange((currVotesChange) => currVotesChange - 1);
-        patchVotesByArtcleId(article_id, (-1))
+    const changeVote = (votesChange) => {
+        setVotesChange((currVotesChange) => currVotesChange + votesChange);
+        patchVotesByArtcleId(article_id, 1)
         .catch(() => {
             setVotesChange((currVotesChange) => 
-                currVotesChange + votesChange)
+                currVotesChange - votesChange)
                 alert("VOTE NOT SUBMITTED")
             
         })
+
     }
-
-
 
     return (
         <section>
             <p>Votes: {votes + votesChange}</p>
-            <button onClick={incVote}>Increase a vote for this article!</button>
-            <button onClick={decVote}>Decrease a vote for this article!</button>
+            <button onClick={()=> changeVote(1)}>Increase a vote for this article!</button>
+            <button onClick={()=> changeVote(-1)}>Decrease a vote for this article!</button>
             
         </section>
     )
